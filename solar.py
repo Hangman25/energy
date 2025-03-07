b14e7e8e4af149345b98559ab62b0cb8f8500def
Hangman25 <yuvrajgill0111@icloud.com>
2025-03-07 10:54:07 -0400

    Update solar.py

--- solar.py
+++ solar.py
@@ -14,7 +14,7 @@ def visualize_csv(file_name):
         # Sidebar: Select Time Range
         time_range = st.sidebar.selectbox("Select Time Range", 
                                           ["Daily", "Weekly", "Monthly", "Quarterly", "6 Months", "9 Months", "12 Months"], 
-                                          index=0)
+                                          index=6)
 
         # Get Current Date
         today = datetime.datetime.today()
