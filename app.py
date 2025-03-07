d43dff3797832f5e9d0c3939b2e533031da9fb43
Hangman25 <yuvrajgill0111@icloud.com>
2025-03-07 10:07:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from metar import show_metar
 from taf import show_taf
 from cloud import show_cloud
 from solar import visualize_csv
-
+from location import show_location_predictions 
 
 st.set_page_config(layout="wide", page_title="🌤️ Energy Prediction Dashboard")
 
 # Sidebar Navigation
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["About", "Prediction","METAR","TAF","Cloud Forecast","Solar Parameters"])
+page = st.sidebar.radio("Go to", ["About", "Prediction", "METAR", "TAF", "Cloud Forecast", "Solar Parameters", "Location"])
 
 if page == "About":
     show_about()
@@ -24,5 +24,6 @@ elif page == "Cloud Forecast":
 elif page == "TAF":
     show_taf()
 elif page == "Solar Parameters":
-    file_name = "csv/solar_2025.csv"  
-    visualize_csv(file_name)  
+    visualize_csv("csv/solar_2025.csv")
+elif page == "Location":
+    show_location_predictions()
